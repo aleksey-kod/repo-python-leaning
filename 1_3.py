a0fde1d9cbae94a1f666a8afddfe808e41edaed1
aleksey-kod <aleksey.kod@gmail.com>
2021-09-30 15:18:41 +0300

    Добавил домашнее задание

---
+++ 1_3.py
@@ -0,0 +1,9 @@
+for i in range(1,101):
+    if 11<= i <=14 or i % 10 >= 5 or i % 10 == 0:
+        print(i,"процентов")
+    elif i % 10 == 1:
+        print(i,"процент")
+    else:
+        print(i,"процента")
+
+
